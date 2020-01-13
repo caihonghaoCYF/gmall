350455d81ef163d24bc3101bb808bb1aba172678
hao <2010733788@qq.com>
2020-01-13 14:39:41 +0800

    项目成功运行

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RefreshScope
 @MapperScan(basePackages = "com.atguigu.gmall.pms.dao")
+@EnableSwagger2
 public class GmallPmsApplication {
 
     public static void main(String[] args) {
